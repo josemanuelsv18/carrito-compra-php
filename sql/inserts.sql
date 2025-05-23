adb3d50752b5483b2d52e1c1538fdd10da20ea3e
Josemanuel Sifontes <josemanuelsv18@gmail.com>
2025-05-22 22:57:22 -0500

    agregado inserts para la base de datos

---
+++ sql/inserts.sql
@@ -0,0 +1,21 @@
+INSERT INTO productos (nombre, descripcion, precio, imagen_url)
+VALUES 
+('Chika Llavero', 'Llaverito super cuquis, para compartir con tu bff. <33', 15.99, 'https://i.postimg.cc/X7rpSXHh/CHIWA.jpg'),
+
+('Hachi Mochila', 'Preciosa mochilita peluche kawaii de Hachiware. :D//', 19.99, 'https://i.postimg.cc/8Pxs1DxC/hachimochila.jpg'),
+
+('Chika Wallet', 'Billetera kawaii coquette de chiikawa. uwu', 25.00, 'https://i.postimg.cc/L5b5gMXN/chika-Wallet.jpg'),
+
+('Chika Notes', 'Set de cuadernos variados de chiikawa, 100pag. ;) ', 10.00, 'https://i.postimg.cc/WzbSb0F0/Chika-Notes.jpg'),
+
+('Usagi Pin', 'Pin del gordo watón Usagi. o.O', 5.00, 'https://i.postimg.cc/HWZVhb2X/UsagiPin.jpg', ),
+
+('Lampara Chiikawa', 'Lamparitas personajes de chiikawa. c:', 17.00, 'https://i.postimg.cc/C51dKKFR/Chika-Lampara.png'),
+
+('Chika Estuchera','Suave lapicera peluche de personajes de Chiikawa!.', 7.00 ,'https://i.postimg.cc/6qjc7Tng/Chika-Estuche.jpg'),
+
+('Hachi Abrigo','Abrigo suavecito de Hachiware, para sentirte asi bien Umarun-chan. B)', 29.00 ,'https://i.postimg.cc/y6vvpptj/Hachi-Abrigo.png'),
+
+('Bolsa Chiikawa','Bolsa peluche redonda de chiikawa: c;;', 16.00 ,'https://i.postimg.cc/W37Xv532/Chika-Bolita.png'),
+
+('Pantuflas Chiikawa','Pantuflas peluche de chiikawa', 12.00 ,'https://i.postimg.cc/pXrkwYtH/Pantu-Chiikawa.png');
